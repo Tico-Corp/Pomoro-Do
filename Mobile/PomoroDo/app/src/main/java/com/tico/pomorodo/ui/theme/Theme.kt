d10d3c0c3f8fd275ee4834663bf9d008da8d3ace
dobbi030 <dmstjs7047@hanmail.net>
2024-06-08 22:37:06 +0900

    [FIX] 테마 색상 미적용 오류 해결
    android 12 이상일 때 dynamic color가 적용되어 지정된 테마 색이 아닌 시스템 컬러가 적용됨. 따라서 dynamic color를 사용하지 않도록 코드를 수정함.
    
    Fixes: TICO-22
    Related to: TICO-11, TICO-20

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/theme/Theme.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/theme/Theme.kt
@@ -2,6 +2,7 @@ package com.tico.pomorodo.ui.theme
 
 import android.app.Activity
 import android.os.Build
+import android.util.Log
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -261,19 +262,10 @@ val unspecified_scheme = ColorFamily(
 @Composable
 fun PomoroDoTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable() () -> Unit,
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
+    val colorScheme = if (darkTheme) darkScheme else lightScheme
 
-        darkTheme -> darkScheme
-        else -> lightScheme
-    }
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
@@ -286,7 +278,6 @@ fun PomoroDoTheme(
     CompositionLocalProvider(LocalPomoroDoTypography provides Typography) {
         MaterialTheme(
             colorScheme = colorScheme,
-            // typography = Typography,
             content = content
         )
     }
