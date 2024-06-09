b5fdb8c75b51f2ae9e1f1f44061f03bade2f99ce
dobbi030 <dmstjs7047@hanmail.net>
2024-06-09 16:39:33 +0900

    [CHORE] text resource 추출
    작성한 텍스트를 rersource로 추출함.
    
    Fixes: TICO-23
    Related to: TICO-20

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/auth/view/LogInScreen.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/auth/view/LogInScreen.kt
@@ -21,9 +21,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.tico.pomorodo.R
 import com.tico.pomorodo.ui.iconpack.IcLogoGoogle
 import com.tico.pomorodo.ui.theme.IconPack
 import com.tico.pomorodo.ui.theme.PomoroDoTheme
@@ -71,12 +73,12 @@ fun LogInButton() {
         ) {
             Icon(
                 imageVector = IconPack.IcLogoGoogle,
-                contentDescription = "logo of google",
+                contentDescription = stringResource(R.string.content_ic_logo_google),
                 modifier = Modifier.size(18.dp),
                 tint = Color.Unspecified
             )
             Text(
-                text = "Log in with Google",
+                text = stringResource(R.string.content_log_in_with_google),
                 color = Color(0xFF1F1F1F),
                 textAlign = TextAlign.Start,
                 style = PomoroDoTheme.typography.robotoMedium14
