3ac038d1c0533c02a89669b4b38fbf06ad2b6df7
dobbi030 <dmstjs7047@hanmail.net>
2024-06-14 23:38:24 +0900

    [FEAT] 텍스트 박스 변경
    닉네임 입력 텍스트 박스에 박스와 텍스트 간 패딩 값을 지정해 주기 위해 TextField -> BasicTextField 로 변경함.
    
    Fixes: TICO-32
    Related to: TICO-20

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/auth/view/SignUpScreen.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/auth/view/SignUpScreen.kt
@@ -3,8 +3,10 @@ package com.tico.pomorodo.ui.auth.view
 import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
@@ -12,16 +14,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -33,6 +37,7 @@ import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.bumptech.glide.load.engine.DiskCacheStrategy
@@ -95,40 +100,52 @@ fun IconDefaultProfile(defaultProfileUri: Uri? = null) {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun ProfileEditText() {
-    val focusManager = LocalFocusManager.current
     var text by rememberSaveable { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
+    val interactionSource = remember { MutableInteractionSource() }
 
-    TextField(
+    BasicTextField(
         value = text,
         onValueChange = { text = it },
         modifier = Modifier
             .fillMaxWidth()
-            .heightIn(min = 0.dp)
-            .background(color = Color.Unspecified),
+            .heightIn(min = 0.dp),
         textStyle = PomoroDoTheme.typography.laundryGothicRegular16,
-        label = { Text(text = "닉네임", fontFamily = laundryGothic) },
-        placeholder = {
-            Text(
-                text = "닉네임을 입력해주세요.",
-                color = PomoroDoTheme.colorScheme.gray50,
-                style = PomoroDoTheme.typography.laundryGothicRegular16
-            )
-        },
         keyboardOptions = KeyboardOptions(
             keyboardType = KeyboardType.Text,
             imeAction = ImeAction.Done
         ),
         keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() }),
-        singleLine = true,
-        shape = RoundedCornerShape(size = 5.dp),
-        colors = TextFieldDefaults.colors(
-            focusedContainerColor = PomoroDoTheme.colorScheme.background,
-            unfocusedContainerColor = PomoroDoTheme.colorScheme.background,
-            focusedIndicatorColor = PomoroDoTheme.colorScheme.primaryContainer,
-            focusedLabelColor = PomoroDoTheme.colorScheme.primaryContainer
+        interactionSource = interactionSource
+    ) { innerTextField ->
+        TextFieldDefaults.DecorationBox(
+            value = text,
+            innerTextField = innerTextField,
+            enabled = true,
+            singleLine = true,
+            visualTransformation = VisualTransformation.None,
+            interactionSource = interactionSource,
+            isError = false,
+            label = { Text(text = "닉네임", fontFamily = laundryGothic) },
+            placeholder = {
+                Text(
+                    text = "닉네임을 입력해주세요.",
+                    color = PomoroDoTheme.colorScheme.gray50,
+                    style = PomoroDoTheme.typography.laundryGothicRegular16
+                )
+            },
+            shape = RoundedCornerShape(size = 5.dp),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = PomoroDoTheme.colorScheme.background,
+                unfocusedContainerColor = PomoroDoTheme.colorScheme.background,
+                focusedIndicatorColor = PomoroDoTheme.colorScheme.primaryContainer,
+                focusedLabelColor = PomoroDoTheme.colorScheme.primaryContainer
+            ),
+            contentPadding = PaddingValues(horizontal = 12.dp, vertical = 14.dp)
         )
-    )
+    }
 }
