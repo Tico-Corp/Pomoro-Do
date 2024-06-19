23f399e2715d67146f6b0728e074e7c5b8f5315d
souk0712 <souk0712@gmail.com>
2024-06-19 16:01:25 +0900

    [FEAT] 리소스 추가
    - 투두 리스트에서 필요한 imageVector 및 string을 추가
    
    Fixes: TICO-30

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/theme/__DarkIconPack.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/theme/__DarkIconPack.kt
@@ -8,12 +8,16 @@ import com.tico.pomorodo.ui.iconpack.darkiconpack.IcArrowFrontDark
 import com.tico.pomorodo.ui.iconpack.darkiconpack.IcBottomMyInfoDark
 import com.tico.pomorodo.ui.iconpack.darkiconpack.IcBottomTimerDark
 import com.tico.pomorodo.ui.iconpack.darkiconpack.IcBottomTodoDark
+import com.tico.pomorodo.ui.iconpack.darkiconpack.IcCalendarDateGreenDark
+import com.tico.pomorodo.ui.iconpack.darkiconpack.IcCalendarDateOrangeDark
 import com.tico.pomorodo.ui.iconpack.darkiconpack.IcCalendarDateRedDark
 import com.tico.pomorodo.ui.iconpack.darkiconpack.IcCalendarDateWhiteDark
+import com.tico.pomorodo.ui.iconpack.darkiconpack.IcCalendarDateYellowDark
 import com.tico.pomorodo.ui.iconpack.darkiconpack.IcCalendarDropDownDark
 import com.tico.pomorodo.ui.iconpack.darkiconpack.IcMoreInfoDark
 import com.tico.pomorodo.ui.iconpack.darkiconpack.IcTodoCheckedDark
 import com.tico.pomorodo.ui.iconpack.darkiconpack.IcTodoGoingDark
+import com.tico.pomorodo.ui.iconpack.darkiconpack.IcTodoMoreInfoDark
 import com.tico.pomorodo.ui.iconpack.darkiconpack.IcTodoUncheckedDark
 
 object DarkIconPack {
@@ -24,15 +28,19 @@ object DarkIconPack {
             IC_BOTTOM_TIMER to IcBottomTimerDark,
             IC_ARROW_FRONT to IcArrowFrontDark,
             IC_ARROW_BACK to IcArrowBackDark,
-            IC_CALENDAR_DATE_RED to IcCalendarDateRedDark,
             IC_CALENDAR_DROP_DOWN to IcCalendarDropDownDark,
-            IC_CALENDAR_DATE_WHITE to IcCalendarDateWhiteDark,
             IC_ADD_TODO to IcAddTodoDark,
             IC_TODO_UNCHECKED to IcTodoUncheckedDark,
             IC_TODO_CHECKED to IcTodoCheckedDark,
             IC_TODO_GOING to IcTodoGoingDark,
+            IC_TODO_MORE_INFO to IcTodoMoreInfoDark,
             IC_MORE_INFO to IcMoreInfoDark,
             IC_ADD_CATEGORY to IcAddCategoryDark,
+            IC_CALENDAR_DATE_WHITE to IcCalendarDateWhiteDark,
+            IC_CALENDAR_DATE_GREEN to IcCalendarDateGreenDark,
+            IC_CALENDAR_DATE_YELLOW to IcCalendarDateYellowDark,
+            IC_CALENDAR_DATE_ORANGE to IcCalendarDateOrangeDark,
+            IC_CALENDAR_DATE_RED to IcCalendarDateRedDark,
         )
     }
 
--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/theme/__LightIconPack.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/theme/__LightIconPack.kt
@@ -8,8 +8,11 @@ import com.tico.pomorodo.ui.iconpack.lighticonpack.IcArrowFrontLight
 import com.tico.pomorodo.ui.iconpack.lighticonpack.IcBottomMyInfoLight
 import com.tico.pomorodo.ui.iconpack.lighticonpack.IcBottomTimerLight
 import com.tico.pomorodo.ui.iconpack.lighticonpack.IcBottomTodoLight
+import com.tico.pomorodo.ui.iconpack.lighticonpack.IcCalendarDateGreenLight
+import com.tico.pomorodo.ui.iconpack.lighticonpack.IcCalendarDateOrangeLight
 import com.tico.pomorodo.ui.iconpack.lighticonpack.IcCalendarDateRedLight
 import com.tico.pomorodo.ui.iconpack.lighticonpack.IcCalendarDateWhiteLight
+import com.tico.pomorodo.ui.iconpack.lighticonpack.IcCalendarDateYellowLight
 import com.tico.pomorodo.ui.iconpack.lighticonpack.IcCalendarDropDownLight
 import com.tico.pomorodo.ui.iconpack.lighticonpack.IcMoreInfoLight
 import com.tico.pomorodo.ui.iconpack.lighticonpack.IcTodoCheckedLight
@@ -25,9 +28,7 @@ object LightIconPack {
             IC_BOTTOM_TIMER to IcBottomTimerLight,
             IC_ARROW_FRONT to IcArrowFrontLight,
             IC_ARROW_BACK to IcArrowBackLight,
-            IC_CALENDAR_DATE_RED to IcCalendarDateRedLight,
             IC_CALENDAR_DROP_DOWN to IcCalendarDropDownLight,
-            IC_CALENDAR_DATE_WHITE to IcCalendarDateWhiteLight,
             IC_ADD_TODO to IcAddTodoLight,
             IC_TODO_UNCHECKED to IcTodoUncheckedLight,
             IC_TODO_CHECKED to IcTodoCheckedLight,
@@ -35,6 +36,11 @@ object LightIconPack {
             IC_TODO_MORE_INFO to IcTodoMoreInfoLight,
             IC_MORE_INFO to IcMoreInfoLight,
             IC_ADD_CATEGORY to IcAddCategoryLight,
+            IC_CALENDAR_DATE_WHITE to IcCalendarDateWhiteLight,
+            IC_CALENDAR_DATE_GREEN to IcCalendarDateGreenLight,
+            IC_CALENDAR_DATE_YELLOW to IcCalendarDateYellowLight,
+            IC_CALENDAR_DATE_ORANGE to IcCalendarDateOrangeLight,
+            IC_CALENDAR_DATE_RED to IcCalendarDateRedLight,
         )
     }
 
