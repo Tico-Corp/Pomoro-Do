a16c97740763dfef917168517ce4be8fa00804a1
souk0712 <souk0712@gmail.com>
2024-06-19 16:08:53 +0900

    [FEAT] 초점을 옮겼을 때 실행하는 확장함수
    - 초점을 옮겼을 때 실행하는 확장함수
    
    Fixes: TICO-30

---
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/common/view/Extensions.kt
@@ -0,0 +1,15 @@
+package com.tico.pomorodo.ui.common.view
+
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
+import androidx.compose.ui.input.pointer.pointerInput
+
+fun Modifier.addFocusCleaner(focusManager: FocusManager, doOnClear: () -> Unit = {}): Modifier {
+    return this.pointerInput(Unit) {
+        detectTapGestures(onTap = {
+            doOnClear()
+            focusManager.clearFocus()
+        })
+    }
+}
