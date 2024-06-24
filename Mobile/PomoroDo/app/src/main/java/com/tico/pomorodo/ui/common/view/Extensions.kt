c0a047adc5b8307ebb8952f2e4a72ec500892c00
souk0712 <souk0712@gmail.com>
2024-06-24 23:23:48 +0900

    [FEAT] 확장 함수 추가
    - 띄어쓰기 제거하는 string 확장 함수
    - user to selectedUser
    - selectedUser to user
    
    Fixes: TICO-31

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/common/view/Extensions.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/common/view/Extensions.kt
@@ -4,12 +4,29 @@ import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.input.pointer.pointerInput
+import com.tico.pomorodo.data.local.entity.SelectedUser
+import com.tico.pomorodo.data.local.entity.User
 
 fun Modifier.addFocusCleaner(focusManager: FocusManager, doOnClear: () -> Unit = {}): Modifier {
     return this.pointerInput(Unit) {
-        detectTapGestures(onTap = {
-            doOnClear()
-            focusManager.clearFocus()
-        })
+        detectTapGestures(
+            onTap = {
+                doOnClear()
+                focusManager.clearFocus()
+            }
+        )
     }
 }
+
+fun String.getNoSpace(): String = this.replace(" ", "")
+
+fun User.toSelectedUser(selected: Boolean = false): SelectedUser =
+    SelectedUser(
+        id = this.id,
+        name = this.name,
+        profileUrl = this.profileUrl,
+        selected = selected
+    )
+
+fun SelectedUser.toUser(): User =
+    User(id = this.id, name = this.name, profileUrl = this.profileUrl)
