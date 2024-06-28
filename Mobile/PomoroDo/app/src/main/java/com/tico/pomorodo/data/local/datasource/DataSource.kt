e9a2c70fefb293b48997ff1392b6c9fcc9fe51be
souk0712 <souk0712@gmail.com>
2024-06-28 16:27:50 +0900

    [FEAT] 임시 타임라인 기록 데이터 추가
    - 임시 타임라인 기록 데이터 추가
    
    Fixes: TICO-35

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/local/datasource/DataSource.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/local/datasource/DataSource.kt
@@ -2,9 +2,12 @@ package com.tico.pomorodo.data.local.datasource
 
 import com.tico.pomorodo.data.model.Category
 import com.tico.pomorodo.data.model.InviteCategory
+import com.tico.pomorodo.data.model.TimeLineData
+import com.tico.pomorodo.data.model.TimeLineType
 import com.tico.pomorodo.data.model.TodoData
 import com.tico.pomorodo.data.model.User
 import com.tico.pomorodo.ui.todo.view.TodoState
+import java.time.LocalDateTime
 
 object DataSource {
     val userList = listOf<User>(
@@ -23,7 +26,7 @@ object DataSource {
         User(id = "13", name = "b"),
     )
 
-    val todoList = listOf<Category>(
+    val categoryList = listOf<Category>(
         Category(
             id = "1",
             title = "카테고리 1",
@@ -95,15 +98,157 @@ object DataSource {
     )
 
     val inviteList = listOf<InviteCategory>(
-        InviteCategory(id = "1", title = "알고리즘 뿌셔!", groupReader ="랑드샤 쿠키"),
-        InviteCategory(id = "2", title = "공작 영애들의 우아한 코딩 궁전", groupReader ="모카커피1"),
-        InviteCategory(id = "3", title = "공작 영애들의 우아한 코딩 궁전", groupReader ="모카커피2"),
-        InviteCategory(id = "4", title = "공작 영애들의 우아한 코딩 궁전", groupReader ="모카커피3"),
-        InviteCategory(id = "5", title = "알고리즘 뿌셔!", groupReader ="모카커피4"),
-        InviteCategory(id = "6", title = "공작 영애들의 우아한 코딩 궁전", groupReader ="모카커피1"),
-        InviteCategory(id = "7", title = "공작 영애들의 우아한 코딩 궁전", groupReader ="모카커피1"),
-        InviteCategory(id = "7", title = "공작 영애들의 우아한 코딩 궁전", groupReader ="모카커피1"),
-        InviteCategory(id = "7", title = "공작 영애들의 우아한 코딩 궁전", groupReader ="모카커피1"),
-        InviteCategory(id = "7", title = "공작 영애들의 우아한 코딩 궁전", groupReader ="모카커피1"),
+        InviteCategory(id = "1", title = "알고리즘 뿌셔!", groupReader = "랑드샤 쿠키"),
+        InviteCategory(id = "2", title = "공작 영애들의 우아한 코딩 궁전", groupReader = "모카커피1"),
+        InviteCategory(id = "3", title = "공작 영애들의 우아한 코딩 궁전", groupReader = "모카커피2"),
+        InviteCategory(id = "4", title = "공작 영애들의 우아한 코딩 궁전", groupReader = "모카커피3"),
+        InviteCategory(id = "5", title = "알고리즘 뿌셔!", groupReader = "모카커피4"),
+        InviteCategory(id = "6", title = "공작 영애들의 우아한 코딩 궁전", groupReader = "모카커피1"),
+        InviteCategory(id = "7", title = "공작 영애들의 우아한 코딩 궁전", groupReader = "모카커피1"),
+        InviteCategory(id = "7", title = "공작 영애들의 우아한 코딩 궁전", groupReader = "모카커피1"),
+        InviteCategory(id = "7", title = "공작 영애들의 우아한 코딩 궁전", groupReader = "모카커피1"),
+        InviteCategory(id = "7", title = "공작 영애들의 우아한 코딩 궁전", groupReader = "모카커피1"),
+    )
+
+    val timeLine = listOf<TimeLineData>(
+        TimeLineData(
+            start = LocalDateTime.of(2024, 6, 27, 5, 0, 0),
+            end = LocalDateTime.of(2024, 6, 27, 7, 55, 45),
+            type = TimeLineType.OUTSIDE,
+        ),
+        TimeLineData(
+            start = LocalDateTime.of(2024, 6, 27, 7, 16, 45),
+            end = LocalDateTime.of(2024, 6, 27, 7, 55, 45),
+            type = TimeLineType.FOCUS,
+            list = listOf(
+                TodoData(
+                    id = "1",
+                    name = "Todo 1",
+                    state = TodoState.GOING,
+                    completeGroupNumber = 4,
+                    likedNumber = 22
+                ),
+                TodoData(
+                    id = "2",
+                    name = "Todo 2",
+                    state = TodoState.GOING,
+                    completeGroupNumber = 4,
+                    likedNumber = 22
+                ),
+            )
+        ),
+        TimeLineData(
+            start = LocalDateTime.of(2024, 6, 27, 7, 55, 45),
+            end = LocalDateTime.of(2024, 6, 27, 8, 0, 45),
+            type = TimeLineType.BREAK,
+        ),
+        TimeLineData(
+            start = LocalDateTime.of(2024, 6, 27, 8, 0, 45),
+            end = LocalDateTime.of(2024, 6, 27, 8, 41, 0),
+            type = TimeLineType.FOCUS,
+            list = listOf(
+                TodoData(
+                    id = "1",
+                    name = "Todo 4",
+                    state = TodoState.GOING,
+                    completeGroupNumber = 4,
+                    likedNumber = 22
+                ),
+                TodoData(
+                    id = "2",
+                    name = "Todo 2",
+                    state = TodoState.CHECKED,
+                    completeGroupNumber = 4,
+                    likedNumber = 22
+                ),
+            )
+        ),
+        TimeLineData(
+            start = LocalDateTime.of(2024, 6, 27, 8, 41, 0),
+            end = LocalDateTime.of(2024, 6, 27, 8, 46, 10),
+            type = TimeLineType.BREAK
+        ),
+        TimeLineData(
+            start = LocalDateTime.of(2024, 6, 27, 8, 46, 10),
+            end = LocalDateTime.of(2024, 6, 27, 9, 50, 34),
+            type = TimeLineType.FOCUS,
+            list = listOf(
+                TodoData(
+                    id = "1",
+                    name = "Todo 4",
+                    state = TodoState.CHECKED,
+                    completeGroupNumber = 4,
+                    likedNumber = 22
+                ),
+                TodoData(
+                    id = "2",
+                    name = "Todo 5",
+                    state = TodoState.CHECKED,
+                    completeGroupNumber = 4,
+                    likedNumber = 22
+                ),
+            )
+        ),
+        TimeLineData(
+            start = LocalDateTime.of(2024, 6, 27, 9, 50, 34),
+            end = LocalDateTime.of(2024, 6, 27, 11, 13, 23),
+            type = TimeLineType.OUTSIDE,
+        ),
+        TimeLineData(
+            start = LocalDateTime.of(2024, 6, 27, 11, 13, 23),
+            end = LocalDateTime.of(2024, 6, 27, 11, 53, 23),
+            type = TimeLineType.FOCUS,
+            list = listOf(
+                TodoData(
+                    id = "1",
+                    name = "Todo 4",
+                    state = TodoState.CHECKED,
+                    completeGroupNumber = 4,
+                    likedNumber = 22
+                ),
+            )
+        ),
+        TimeLineData(
+            start = LocalDateTime.of(2024, 6, 27, 11, 53, 23),
+            end = LocalDateTime.of(2024, 6, 27, 11, 58, 23),
+            type = TimeLineType.BREAK
+        ),
+        TimeLineData(
+            start = LocalDateTime.of(2024, 6, 27, 11, 58, 23),
+            end = LocalDateTime.of(2024, 6, 27, 12, 0, 46),
+            type = TimeLineType.FOCUS,
+            list = listOf()
+        ),
+    )
+
+    val todoList = listOf(
+        TodoData(
+            id = "1",
+            name = "Todo 1",
+            state = TodoState.UNCHECKED,
+            completeGroupNumber = 4,
+            likedNumber = 22
+        ),
+        TodoData(
+            id = "2",
+            name = "Todo 1",
+            state = TodoState.CHECKED,
+            completeGroupNumber = 1,
+            likedNumber = 22
+        ),
+        TodoData(
+            id = "3",
+            name = "Todo 1",
+            state = TodoState.GOING,
+            completeGroupNumber = 3,
+            likedNumber = 22
+        ),
+        TodoData(
+            id = "4",
+            name = "Todo 1",
+            state = TodoState.UNCHECKED,
+            completeGroupNumber = 0,
+            likedNumber = 0
+        )
     )
 }
