1889bef1cc6874ea72f043e07cde13d6dbbc159e
dobbi030 <dmstjs7047@hanmail.net>
2024-06-28 18:02:08 +0900

    [REFACTOR] CustomTimeText 함수 병합
    CustomTimeText의 오버로딩 함수를 병합
    
    Fixes: TICO-44
    Related to: TICO-33

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/common/view/CustomTimeText.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/common/view/CustomTimeText.kt
@@ -1,13 +1,11 @@
 package com.tico.pomorodo.ui.common.view
 
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,209 +14,23 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.Dp
 import com.tico.pomorodo.R
-import java.util.Locale
 
 @Composable
 fun CustomTimeText(
     title: String,
     hour: Int,
     minute: Int,
-    second: Int,
-    textColor: Color,
-    spaceDp: Dp,
-    textStyle: TextStyle,
-) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = title,
-            color = textColor,
-            textAlign = TextAlign.Center,
-            style = textStyle
-        )
-
-        Spacer(modifier = Modifier.height(spaceDp))
-
-        Text(
-            text = String.format(
-                Locale.KOREA,
-                stringResource(id = R.string.format_hour_minute_second),
-                hour,
-                minute,
-                second
-            ),
-            color = textColor,
-            textAlign = TextAlign.Center,
-            style = textStyle
-        )
-    }
-}
-
-@Composable
-fun CustomTimeText(
-    title: String,
-    hour: Int,
-    minute: Int,
-    second: Int,
-    textColor: Color,
-    spaceDp: Dp,
-    textStyle: TextStyle,
-    onClick: () -> Unit
-) {
-    Column(
-        modifier = Modifier.clickableWithoutRipple { onClick() },
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = title,
-            color = textColor,
-            textAlign = TextAlign.Center,
-            style = textStyle
-        )
-
-        Spacer(modifier = Modifier.height(spaceDp))
-
-        Text(
-            text = String.format(
-                Locale.KOREA,
-                stringResource(id = R.string.format_hour_minute_second),
-                hour,
-                minute,
-                second
-            ),
-            color = textColor,
-            textAlign = TextAlign.Center,
-            style = textStyle
-        )
-    }
-}
-
-@Composable
-fun CustomTimeText(
-    title: String,
-    hour: Int,
-    minute: Int,
-    second: Int,
+    second: Int? = null,
     textColor: Color,
     spaceDp: Dp,
     titleTextStyle: TextStyle,
-    contentTextStyle: TextStyle,
+    contentTextStyle: TextStyle = titleTextStyle,
+    onClick: () -> Unit = {}
 ) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = title,
-            color = textColor,
-            textAlign = TextAlign.Center,
-            style = titleTextStyle
-        )
+    val text =
+        if (second == null) stringResource(id = R.string.format_hour_minute, hour, minute)
+        else stringResource(id = R.string.format_hour_minute_second, hour, minute, second)
 
-        Spacer(modifier = Modifier.height(spaceDp))
-
-        Text(
-            text = String.format(
-                Locale.KOREA,
-                stringResource(id = R.string.format_hour_minute_second),
-                hour,
-                minute,
-                second
-            ),
-            color = textColor,
-            textAlign = TextAlign.Center,
-            style = contentTextStyle
-        )
-    }
-}
-
-@Composable
-fun CustomTimeText(
-    title: String,
-    hour: Int,
-    minute: Int,
-    textColor: Color,
-    spaceDp: Dp,
-    textStyles: TextStyle,
-) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = title,
-            color = textColor,
-            textAlign = TextAlign.Center,
-            style = textStyles
-        )
-
-        Spacer(modifier = Modifier.height(spaceDp))
-
-        Text(
-            text = String.format(
-                Locale.KOREA,
-                stringResource(id = R.string.format_hour_minute),
-                hour,
-                minute
-            ),
-            color = textColor,
-            textAlign = TextAlign.Center,
-            style = textStyles
-        )
-    }
-}
-
-@Composable
-fun CustomTimeText(
-    title: String,
-    hour: Int,
-    minute: Int,
-    textColor: Color,
-    spaceDp: Dp,
-    titleTextStyle: TextStyle,
-    contentTextStyle: TextStyle,
-) {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = title,
-            color = textColor,
-            textAlign = TextAlign.Center,
-            style = titleTextStyle
-        )
-
-        Spacer(modifier = Modifier.height(spaceDp))
-
-        Text(
-            text = String.format(
-                Locale.KOREA,
-                stringResource(id = R.string.format_hour_minute),
-                hour,
-                minute,
-            ),
-            color = textColor,
-            textAlign = TextAlign.Center,
-            style = contentTextStyle
-        )
-    }
-}
-
-@Composable
-fun CustomTimeText(
-    title: String,
-    hour: Int,
-    minute: Int,
-    textColor: Color,
-    spaceDp: Dp,
-    titleTextStyle: TextStyle,
-    contentTextStyle: TextStyle,
-    onClick: () -> Unit
-) {
     Column(
         modifier = Modifier.clickableWithoutRipple { onClick() },
         verticalArrangement = Arrangement.Center,
@@ -234,12 +46,7 @@ fun CustomTimeText(
         Spacer(modifier = Modifier.height(spaceDp))
 
         Text(
-            text = String.format(
-                Locale.KOREA,
-                stringResource(id = R.string.format_hour_minute),
-                hour,
-                minute,
-            ),
+            text = text,
             color = textColor,
             textAlign = TextAlign.Center,
             style = contentTextStyle
