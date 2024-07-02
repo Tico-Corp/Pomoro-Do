c166cdff1fbbe36983716ef3159208aa1d60c0af
김부경 <sa01023@naver.com>
2024-07-03 01:49:40 +0900

    [FEAT] SecurityConfig 기본 설정 작성
    SecurityConfig 클래스 생성 및 기본적인 시큐리티 설정 구현
    
    Resolves: TICO-160
    Related to: TICO-161, TICO-163

---
+++ backend/pomoro-do/src/main/java/com/tico/pomoro_do/global/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.tico.pomoro_do.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    // 허용 주소
+    private static final String[] WHITE_LIST = {
+            "/login", "/", "/join"
+    };
+
+    // 비밀번호 암호화
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder()
+    {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //비활성화
+        http
+                .csrf(AbstractHttpConfigurer::disable) // CSRF 보안 disable - JWT 방식으로 관리하여 세션을 사용하지 않으므로 방어할 필요X
+                .formLogin(AbstractHttpConfigurer::disable) //From 로그인 방식 disable - JWT 방식으로 관리
+                .httpBasic(AbstractHttpConfigurer::disable); //HTTP Basic 인증 방식 disable - JWT 방식으로 관리
+//                .oauth2Login(Customizer.withDefaults()); //oauth2 사용하면 적용
+
+        //세션 설정 : STATELESS - 사용 안함
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        //경로별 인가 작업 - 특정 경로에 대한 권한 관리 옵션
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(WHITE_LIST).permitAll() // 해당 경로에 대해 모든 권한 허용
+                        .requestMatchers("/admin").hasRole("ADMIN") // 어드민 권한을 가진 사용자만 접근 가능
+                        .anyRequest().authenticated()); // 그 외의 요청에 대해 로그인한 사용자만 접근 가능
+
+        return http.build();
+    }
+}
