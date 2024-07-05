fbefd9029affc39a027596938e493b6f268d74ab
souk0712 <souk0712@gmail.com>
2024-07-06 01:07:36 +0900

    [FEAT] google idToken 저장 방법 변경 및 authToken을 구현
    - google idToken을 viewmodel 저장으로 변경
    - authToken을 sharedPreference에 저장하게 함
    
    Fixes: TICO-133

---
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/domain/usecase/ClearTokenUseCase.kt
@@ -0,0 +1,14 @@
+package com.tico.pomorodo.domain.usecase
+
+import com.tico.pomorodo.domain.repository.AuthRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class ClearTokenUseCase @Inject constructor(private val authRepository: AuthRepository) {
+
+    suspend operator fun invoke() =
+        withContext(Dispatchers.IO) {
+            authRepository.clearToken()
+        }
+}
