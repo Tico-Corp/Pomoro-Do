a718ecf97ef2d53a87a3fecf9d431c281d95766f
souk0712 <souk0712@gmail.com>
2024-07-08 12:33:14 +0900

    [FEAT] 기록 화면의 데이터 관리를 위한 HistoryViewModel 생성
    Fixes: TICO-172

---
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/history/viewmodel/HistoryViewModel.kt
@@ -0,0 +1,30 @@
+package com.tico.pomorodo.ui.history.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.tico.pomorodo.data.local.datasource.DataSource
+import com.tico.pomorodo.data.model.TimeLineData
+import com.tico.pomorodo.data.model.TodoData
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
+
+class HistoryViewModel @Inject constructor() : ViewModel() {
+    private var _timeLine = MutableStateFlow<List<TimeLineData>>(DataSource.timeLine)
+    val timeLine: StateFlow<List<TimeLineData>>
+        get() = _timeLine.asStateFlow()
+
+    private var _todoList = MutableStateFlow<List<TodoData>>(DataSource.todoList)
+    val todoList: StateFlow<List<TodoData>>
+        get() = _todoList.asStateFlow()
+
+    private var dialogSelectedIndex = MutableStateFlow(-1)
+
+    fun setTodoEdit(newTodoList: List<TodoData>) {
+        _todoList.value = newTodoList
+    }
+
+    fun setDialogSelectedIndex(index: Int) {
+        dialogSelectedIndex.value = index
+    }
+}
