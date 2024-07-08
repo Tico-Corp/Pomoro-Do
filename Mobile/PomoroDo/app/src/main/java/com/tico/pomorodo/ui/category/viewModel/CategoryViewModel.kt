f0315d929378562bd497985374bf5097ae430b0c
souk0712 <souk0712@gmail.com>
2024-07-08 12:23:00 +0900

    [FEAT] 데이터 관리를 CategoryViewModel로 이동
    Fixes: TICO-172

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/category/viewModel/CategoryViewModel.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/category/viewModel/CategoryViewModel.kt
@@ -2,15 +2,25 @@ package com.tico.pomorodo.ui.category.viewModel
 
 import androidx.lifecycle.ViewModel
 import com.tico.pomorodo.data.local.datasource.DataSource
+import com.tico.pomorodo.data.model.Category
+import com.tico.pomorodo.data.model.InviteCategory
 import com.tico.pomorodo.data.model.OpenSettings
 import com.tico.pomorodo.data.model.SelectedUser
 import com.tico.pomorodo.data.model.User
-import com.tico.pomorodo.ui.common.view.toUser
+import com.tico.pomorodo.ui.common.view.toSelectedUser
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
 class CategoryViewModel : ViewModel() {
+
+    private var _categoryList = MutableStateFlow(DataSource.categoryList)
+    val categoryList: StateFlow<List<Category>>
+        get() = _categoryList.asStateFlow()
+
+    private var _inviteGroupCategoryList = MutableStateFlow(DataSource.inviteList)
+    val inviteGroupCategoryList: StateFlow<List<InviteCategory>>
+        get() = _inviteGroupCategoryList.asStateFlow()
 
     private var _openSettingOption = MutableStateFlow(OpenSettings.FULL)
     val openSettingOption: StateFlow<OpenSettings>
@@ -28,6 +38,11 @@ class CategoryViewModel : ViewModel() {
     val groupMembers: StateFlow<List<User>>
         get() = _groupMembers.asStateFlow()
 
+    private var _selectedGroupMembers =
+        MutableStateFlow<List<SelectedUser>>(groupMembers.value.map { it.toSelectedUser() })
+    val selectedGroupMembers: StateFlow<List<SelectedUser>>
+        get() = _selectedGroupMembers.asStateFlow()
+
     fun setOpenSettingOption(newOption: OpenSettings) {
         _openSettingOption.value = newOption
     }
@@ -40,9 +55,8 @@ class CategoryViewModel : ViewModel() {
         _type.value = type
     }
 
-    fun setGroupMembers(newList: List<SelectedUser>) {
-        _groupMembers.value =
-            newList.toMutableList().filter { it.selected }.map { it.toUser() }
+    fun setSelectedGroupMembers(newList: List<SelectedUser>) {
+        _selectedGroupMembers.value = newList
     }
 
     fun validateInput(): Boolean = title.value.isNotBlank()
