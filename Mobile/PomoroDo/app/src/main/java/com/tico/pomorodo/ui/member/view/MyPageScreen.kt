ff18526338ab2cf8ed890fc55289f9f6aef5f01c
River is line <dmstjs7047@hanmail.net>
2024-07-08 17:53:27 +0900

    [FEAT] 마이 페이지 UI 구현
    Resolves: TICO-66
    Related to: TICO-61

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/member/view/MyPageScreen.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/member/view/MyPageScreen.kt
@@ -1,8 +1,258 @@
 package com.tico.pomorodo.ui.member.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.tico.pomorodo.R
+import com.tico.pomorodo.ui.common.view.CustomSwitch
+import com.tico.pomorodo.ui.common.view.Profile
+import com.tico.pomorodo.ui.common.view.SimpleIcon
+import com.tico.pomorodo.ui.common.view.SimpleText
+import com.tico.pomorodo.ui.common.view.clickableWithRipple
+import com.tico.pomorodo.ui.common.view.clickableWithoutRipple
+import com.tico.pomorodo.ui.iconpack.commonIconPack.IcSetting
+import com.tico.pomorodo.ui.theme.IC_DROP_DOWN
+import com.tico.pomorodo.ui.theme.IconPack
+import com.tico.pomorodo.ui.theme.PomoroDoTheme
+
+const val tempUrl = "https://avatars.githubusercontent.com/u/51740252?s=48&v=4"
+const val tempName = "모카커피짱귀엽"
 
 @Composable
 fun MyPageScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 18.dp, end = 18.dp, top = 24.dp),
+        horizontalAlignment = Alignment.End
+    ) {
+        Icon(
+            imageVector = IconPack.IcSetting,
+            contentDescription = stringResource(R.string.content_ic_setting),
+            modifier = Modifier
+                .size(28.dp)
+                .clickableWithRipple(
+                    roundedCornerRadius = 150.dp,
+                    onClick = { /*TODO: 설정 화면이 나타남*/ }),
+            tint = Color.Unspecified
+        )
 
+        MyProfile(
+            url = tempUrl,
+            userName = tempName,
+            followingCount = 4,
+            followerCount = 2
+        )
+
+        Spacer(modifier = Modifier.height(28.dp))
+
+        MyPageMenuList()
+    }
+}
+
+@Composable
+fun MyProfile(url: String, userName: String, followingCount: Int, followerCount: Int) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(12.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Profile(
+            url = url,
+            modifier = Modifier.clickableWithoutRipple { /*TODO: 프로필 수정 화면이 나타남*/ },
+            size = 60
+        )
+
+        Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
+            Text(
+                text = userName,
+                modifier = Modifier.clickableWithoutRipple { /*TODO: 위와 동일. 프로필 수정 화면이 나타남.*/ },
+                color = PomoroDoTheme.colorScheme.onBackground,
+                textAlign = TextAlign.Start,
+                style = PomoroDoTheme.typography.laundryGothicRegular20
+            )
+
+            Row(
+                modifier = Modifier.clickableWithoutRipple { /*TODO: 팔로우 화면이 나타남.*/ },
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                FollowText(title = stringResource(R.string.title_following), count = followingCount)
+                FollowText(title = stringResource(R.string.title_follower), count = followerCount)
+            }
+        }
+    }
+}
+
+@Composable
+fun FollowText(title: String, count: Int) {
+    Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+        val textStyle = PomoroDoTheme.typography.laundryGothicRegular14
+        val textColor = PomoroDoTheme.colorScheme.onBackground
+
+        SimpleText(text = title, style = textStyle, color = textColor)
+        SimpleText(text = count.toString(), style = textStyle, color = textColor)
+    }
+}
+
+@Composable
+fun MyPageMenuList() {
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(18.dp)
+    ) {
+        Surface(
+            shape = RoundedCornerShape(10.dp),
+            color = PomoroDoTheme.colorScheme.myPageMenuBackgroundColor
+        ) {
+            Text(
+                text = stringResource(id = R.string.title_invite),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickableWithRipple(
+                        roundedCornerRadius = 10.dp,
+                        onClick = {/*TODO: 친구 초대 기능 구현*/ })
+                    .padding(horizontal = 18.dp, vertical = 16.dp),
+                color = PomoroDoTheme.colorScheme.onBackground,
+                textAlign = TextAlign.Start,
+                style = PomoroDoTheme.typography.laundryGothicRegular14
+            )
+        }
+
+        Surface(
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(10.dp),
+            color = PomoroDoTheme.colorScheme.myPageMenuBackgroundColor
+        ) {
+            val lineSpacer: @Composable () -> Unit = {
+                Spacer(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(1.dp)
+                        .background(PomoroDoTheme.colorScheme.outlineVariant)
+                )
+            }
+
+            Column(modifier = Modifier.padding(horizontal = 18.dp, vertical = 4.dp)) {
+                SetAlarm(title = stringResource(R.string.title_alarm_concentration))
+
+                lineSpacer()
+
+                SetAlarm(title = stringResource(R.string.title_alarm_break))
+
+                lineSpacer()
+
+                val (checked1, setChecked1) = remember { mutableStateOf(false) }
+                SetStopWatchMode(
+                    title = stringResource(R.string.title_stopwatch_concentration),
+                    checked = checked1
+                ) {
+                    setChecked1(it)
+                }
+
+                lineSpacer()
+
+                val (checked2, setChecked2) = remember { mutableStateOf(false) }
+                SetStopWatchMode(
+                    title = stringResource(R.string.title_stopwatch_break),
+                    checked = checked2
+                ) {
+                    setChecked2(it)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SetAlarm(title: String) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = title,
+            color = PomoroDoTheme.colorScheme.onBackground,
+            textAlign = TextAlign.Start,
+            style = PomoroDoTheme.typography.laundryGothicRegular14
+        )
+
+        Row(
+            modifier = Modifier
+                .clickableWithRipple(
+                    roundedCornerRadius = 150.dp,
+                    onClick = {/*TODO: 타이머 종료 알림 설정 화면 구현*/ })
+                .padding(top = 12.dp, bottom = 12.dp, start = 12.dp),
+            horizontalArrangement = Arrangement.spacedBy(5.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = stringResource(R.string.content_alarm_option),
+                color = PomoroDoTheme.colorScheme.onBackground,
+                textAlign = TextAlign.End,
+                style = PomoroDoTheme.typography.laundryGothicRegular14
+            )
+            SimpleIcon(
+                size = 15,
+                imageVector = PomoroDoTheme.iconPack[IC_DROP_DOWN]!!,
+                contentDescriptionId = R.string.content_ic_drop_down
+            )
+        }
+    }
+}
+
+@Composable
+fun SetStopWatchMode(
+    title: String,
+    checked: Boolean,
+    onCheckChanged: (Boolean) -> Unit
+) {
+    val content = if (checked) stringResource(R.string.content_stopwatch_option_true)
+    else stringResource(R.string.content_stopwatch_option_false)
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 12.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column(verticalArrangement = Arrangement.spacedBy(4.dp)) {
+            Text(
+                text = title,
+                color = PomoroDoTheme.colorScheme.onBackground,
+                textAlign = TextAlign.Start,
+                style = PomoroDoTheme.typography.laundryGothicRegular14
+            )
+
+            Text(
+                text = content,
+                color = PomoroDoTheme.colorScheme.outline,
+                textAlign = TextAlign.Start,
+                style = PomoroDoTheme.typography.laundryGothicRegular10
+            )
+        }
+
+        CustomSwitch(checked = checked, onCheckedChange = onCheckChanged)
+    }
 }
