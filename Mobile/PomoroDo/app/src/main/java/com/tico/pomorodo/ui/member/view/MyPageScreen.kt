f0ff9f84a329e1b30a60c8db7761048139ec0e66
River is line <dmstjs7047@hanmail.net>
2024-07-10 01:16:52 +0900

    [CHORE] unused import 삭제
    Resolves: TICO-67
    Related to: TICO-61

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/member/view/MyPageScreen.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/member/view/MyPageScreen.kt
@@ -1,7 +1,6 @@
 package com.tico.pomorodo.ui.member.view
 
 import android.net.Uri
-import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
