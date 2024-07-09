ce21a38ea9427a4f1afbaf7049e70d1e908f0c09
River is line <dmstjs7047@hanmail.net>
2024-07-09 15:55:59 +0900

    [FEAT] 타이머 종료 알림 설정 bottom sheet 구현
    Resolves: TICO-66
    Related to: TICO-61

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/member/view/MyPageScreen.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/member/view/MyPageScreen.kt
@@ -46,6 +46,20 @@ fun MyPageScreen(navigateToModifyProfile: () -> Unit) {
     val name by authViewModel.name.collectAsState()
     val profileUri by authViewModel.profile.collectAsState()
 
+fun MyPageScreen() {
+    val (concentrationAlarmBottomSheet, setConcentrationAlarmBottomSheet) = remember {
+        mutableStateOf(
+            false
+        )
+    }
+    val (breakAlarmBottomSheet, setBreakAlarmBottomSheet) = remember { mutableStateOf(false) }
+    val (concentrationAlarmOption, setConcentrationAlarmOption) = remember {
+        mutableStateOf(
+            AlarmOptions.Sound
+        )
+    }
+    val (breakAlarmOption, setBreakAlarmOption) = remember { mutableStateOf(AlarmOptions.Sound) }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -73,7 +87,36 @@ fun MyPageScreen(navigateToModifyProfile: () -> Unit) {
 
         Spacer(modifier = Modifier.height(28.dp))
 
-        MyPageMenuList()
+        MyPageMenuList(
+            concentrationAlarmOption,
+            breakAlarmOption,
+            onConcentrationAlarmClicked = { setConcentrationAlarmBottomSheet(true) },
+            onBreakAlarmClicked = { setBreakAlarmBottomSheet(true) }
+        )
+
+        if (concentrationAlarmBottomSheet) {
+            SettingAlarmBottomSheet(
+                title = stringResource(id = R.string.title_alarm_concentration),
+                initialSelect = concentrationAlarmOption,
+                onDismissRequest = { setConcentrationAlarmBottomSheet(false) },
+                onConfirmation = { alarmOptions ->
+                    setConcentrationAlarmOption(alarmOptions)
+                    setConcentrationAlarmBottomSheet(false)
+                }
+            )
+        }
+
+        if (breakAlarmBottomSheet) {
+            SettingAlarmBottomSheet(
+                title = stringResource(id = R.string.title_alarm_break),
+                initialSelect = breakAlarmOption,
+                onDismissRequest = { setBreakAlarmBottomSheet(false) },
+                onConfirmation = { alarmOptions ->
+                    setBreakAlarmOption(alarmOptions)
+                    setBreakAlarmBottomSheet(false)
+                }
+            )
+        }
     }
 }
 
@@ -128,7 +171,12 @@ fun FollowText(title: String, count: Int) {
 }
 
 @Composable
-fun MyPageMenuList() {
+fun MyPageMenuList(
+    concentrationAlarmOptions: AlarmOptions,
+    breakAlarmOptions: AlarmOptions,
+    onConcentrationAlarmClicked: () -> Unit,
+    onBreakAlarmClicked: () -> Unit
+) {
     Column(
         modifier = Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(18.dp)
@@ -166,11 +214,19 @@ fun MyPageMenuList() {
             }
 
             Column(modifier = Modifier.padding(horizontal = 18.dp, vertical = 4.dp)) {
-                SetAlarm(title = stringResource(R.string.title_alarm_concentration))
+                SetAlarm(
+                    title = stringResource(R.string.title_alarm_concentration),
+                    alarmOptions = concentrationAlarmOptions,
+                    onClick = onConcentrationAlarmClicked
+                )
 
                 lineSpacer()
 
-                SetAlarm(title = stringResource(R.string.title_alarm_break))
+                SetAlarm(
+                    title = stringResource(R.string.title_alarm_break),
+                    alarmOptions = breakAlarmOptions,
+                    onClick = onBreakAlarmClicked
+                )
 
                 lineSpacer()
 
@@ -197,7 +253,7 @@ fun MyPageMenuList() {
 }
 
 @Composable
-fun SetAlarm(title: String) {
+fun SetAlarm(title: String, alarmOptions: AlarmOptions, onClick: () -> Unit) {
     Row(
         modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween,
@@ -212,15 +268,13 @@ fun SetAlarm(title: String) {
 
         Row(
             modifier = Modifier
-                .clickableWithRipple(
-                    roundedCornerRadius = 150.dp,
-                    onClick = {/*TODO: 타이머 종료 알림 설정 화면 구현*/ })
+                .clickableWithRipple(roundedCornerRadius = 150.dp, onClick = onClick)
                 .padding(top = 12.dp, bottom = 12.dp, start = 12.dp),
             horizontalArrangement = Arrangement.spacedBy(5.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Text(
-                text = stringResource(R.string.content_alarm_option),
+                text = stringResource(alarmOptions.title),
                 color = PomoroDoTheme.colorScheme.onBackground,
                 textAlign = TextAlign.End,
                 style = PomoroDoTheme.typography.laundryGothicRegular14
