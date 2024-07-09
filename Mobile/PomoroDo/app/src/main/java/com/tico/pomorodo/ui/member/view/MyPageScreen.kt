7e406b8603d8bc616efa244fcffbd6c8de869c78
River is line <dmstjs7047@hanmail.net>
2024-07-10 01:25:49 +0900

    [FIX] merge conflict
    Resolves: TICO-67
    Related to: TICO-61

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/member/view/MyPageScreen.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/member/view/MyPageScreen.kt
@@ -1,7 +1,6 @@
 package com.tico.pomorodo.ui.member.view
 
 import android.net.Uri
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
