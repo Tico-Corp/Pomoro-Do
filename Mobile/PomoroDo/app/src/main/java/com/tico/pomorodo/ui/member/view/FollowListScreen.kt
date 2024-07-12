702a72603d111eb77395a7f583354087791ed561
River is line <dmstjs7047@hanmail.net>
2024-07-13 04:24:11 +0900

    [FEAT] 팔로우 목록 화면 구현
    Resolves: TICO-68
    Related to: TICO-61

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/member/view/FollowListScreen.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/member/view/FollowListScreen.kt
@@ -1,43 +1,73 @@
 package com.tico.pomorodo.ui.member.view
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.glide.GlideImage
 import com.tico.pomorodo.R
+import com.tico.pomorodo.domain.model.Follow
+import com.tico.pomorodo.ui.common.view.CustomTextButton
 import com.tico.pomorodo.ui.common.view.SimpleIconButton
+import com.tico.pomorodo.ui.member.viewmodel.FollowViewModel
 import com.tico.pomorodo.ui.theme.IC_ARROW_BACK
 import com.tico.pomorodo.ui.theme.PomoroDoTheme
+import kotlinx.coroutines.launch
 
-@Preview
 @Composable
 fun FollowListScreen() {
-    var selectedTabIndex by remember { mutableIntStateOf(0) }
+    val followViewModel: FollowViewModel = hiltViewModel()
+    val followerList by followViewModel.followerList.collectAsState()
+    val followingList by followViewModel.followingList.collectAsState()
+    val pagerState = rememberPagerState(pageCount = { 2 })
+    val coroutineScope = rememberCoroutineScope()
 
-    PomoroDoTheme {
-        Column {
-            TopAppBarWithSingleButton()
+    Column(modifier = Modifier.background(color = PomoroDoTheme.colorScheme.background)) {
+        TopAppBarWithSingleButton()
 
-            FollowTabRow(selectedTabIndex = selectedTabIndex) { index ->
-                selectedTabIndex = index
-            }
+        FollowTabRow(selectedTabIndex = pagerState.currentPage) { index ->
+            coroutineScope.launch { pagerState.animateScrollToPage(index) }
+        }
+
+        HorizontalPager(state = pagerState) { page ->
+            if (page == 0) FollowingList(followList = followerList.toMutableStateList())
+            else FollowerList(followList = followingList.toMutableStateList())
         }
     }
 }
@@ -47,7 +77,6 @@ fun TopAppBarWithSingleButton() {
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .background(color = PomoroDoTheme.colorScheme.background)
             .padding(start = 18.dp, end = 18.dp, top = 24.dp, bottom = 14.dp),
         contentAlignment = Alignment.CenterStart
     ) {
@@ -60,7 +89,7 @@ fun TopAppBarWithSingleButton() {
         )
 
         Text(
-            text = stringResource(R.string.title_follow_list),
+            text = stringResource(R.string.title_follow),
             modifier = Modifier.fillMaxWidth(),
             color = PomoroDoTheme.colorScheme.onBackground,
             textAlign = TextAlign.Center,
@@ -109,6 +138,111 @@ fun FollowTabRow(selectedTabIndex: Int, onSelectedTabIndexChange: (Int) -> Unit)
                     style = PomoroDoTheme.typography.laundryGothicRegular16
                 )
             }
+        )
+    }
+}
+
+@Composable
+fun FollowingList(followList: SnapshotStateList<Follow>) {
+    LazyColumn(
+        modifier = Modifier
+            .padding(horizontal = 18.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(18.dp)
+    ) {
+        itemsIndexed(items = followList) { index, user ->
+            if (index == 0)
+                Spacer(modifier = Modifier.height(18.dp))
+
+            FollowItem(
+                user = user,
+                followButtonText = stringResource(id = R.string.title_following),
+                unFollowButtonText = stringResource(id = R.string.title_follow),
+                followButtonContainerColor = PomoroDoTheme.colorScheme.gray90,
+                followButtonContentColor = PomoroDoTheme.colorScheme.gray20,
+                unFollowButtonContainerColor = PomoroDoTheme.colorScheme.primaryContainer,
+                unFollowButtonContentColor = Color.White,
+                onClick = { followList[index] = user.copy(isFollowing = !user.isFollowing) }
+            )
+        }
+    }
+}
+
+@Composable
+fun FollowerList(followList: SnapshotStateList<Follow>) {
+    LazyColumn(
+        modifier = Modifier
+            .padding(top = 18.dp, start = 18.dp, end = 18.dp)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(18.dp)
+    ) {
+        items(items = followList) { user ->
+            FollowItem(
+                user = user,
+                followButtonText = stringResource(id = R.string.content_delete),
+                followButtonContainerColor = PomoroDoTheme.colorScheme.gray90,
+                followButtonContentColor = PomoroDoTheme.colorScheme.gray20,
+                onClick = { followList.remove(user) }
+            )
+        }
+    }
+}
+
+@Composable
+fun FollowItem(
+    user: Follow,
+    followButtonText: String,
+    unFollowButtonText: String? = null,
+    followButtonContainerColor: Color,
+    followButtonContentColor: Color,
+    unFollowButtonContainerColor: Color? = null,
+    unFollowButtonContentColor: Color? = null,
+    onClick: () -> Unit
+) {
+    val buttonText =
+        if (user.isFollowing) followButtonText
+        else unFollowButtonText ?: followButtonText
+    val buttonContainerColor =
+        if (user.isFollowing) followButtonContainerColor
+        else unFollowButtonContainerColor ?: followButtonContainerColor
+    val buttonContentColor =
+        if (user.isFollowing) followButtonContentColor
+        else unFollowButtonContentColor ?: followButtonContentColor
+
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        GlideImage(
+            imageModel = { user.profileUrl },
+            modifier = Modifier
+                .size(48.dp)
+                .clip(shape = CircleShape),
+            requestOptions = { RequestOptions().diskCacheStrategy(DiskCacheStrategy.AUTOMATIC) },
+            imageOptions = ImageOptions(
+                contentScale = ContentScale.Crop,
+                alignment = Alignment.Center
+            )
+        )
+
+        Text(
+            text = user.name,
+            color = PomoroDoTheme.colorScheme.onBackground,
+            textAlign = TextAlign.Start,
+            style = PomoroDoTheme.typography.laundryGothicRegular18
+        )
+
+        Spacer(modifier = Modifier.weight(1f))
+
+        CustomTextButton(
+            text = buttonText,
+            containerColor = buttonContainerColor,
+            contentColor = buttonContentColor,
+            textStyle = PomoroDoTheme.typography.laundryGothicRegular16,
+            verticalPadding = 8.dp,
+            horizontalPadding = 15.dp,
+            onClick = onClick
         )
     }
 }
