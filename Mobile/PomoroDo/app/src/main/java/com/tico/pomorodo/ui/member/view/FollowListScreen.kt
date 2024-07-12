97033b88e618e506bf06e593342ee0a8e52e8eeb
River is line <dmstjs7047@hanmail.net>
2024-07-13 01:48:09 +0900

    [FEAT] 팔로우 메뉴 탭 구현
    Resolves: TICO-68
    Related to: TICO-61

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/member/view/FollowListScreen.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/member/view/FollowListScreen.kt
@@ -5,8 +5,16 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -21,14 +29,19 @@ import com.tico.pomorodo.ui.theme.PomoroDoTheme
 @Preview
 @Composable
 fun FollowListScreen() {
+    var selectedTabIndex by remember { mutableIntStateOf(0) }
+
     PomoroDoTheme {
         Column {
             TopAppBarWithSingleButton()
+
+            FollowTabRow(selectedTabIndex = selectedTabIndex) { index ->
+                selectedTabIndex = index
+            }
         }
     }
 }
 
-@Preview
 @Composable
 fun TopAppBarWithSingleButton() {
     Box(
@@ -52,6 +65,50 @@ fun TopAppBarWithSingleButton() {
             color = PomoroDoTheme.colorScheme.onBackground,
             textAlign = TextAlign.Center,
             style = PomoroDoTheme.typography.laundryGothicBold20
+        )
+    }
+}
+
+@Composable
+fun FollowTabRow(selectedTabIndex: Int, onSelectedTabIndexChange: (Int) -> Unit) {
+    val textColor = PomoroDoTheme.colorScheme.onBackground
+
+    TabRow(
+        selectedTabIndex = selectedTabIndex,
+        containerColor = PomoroDoTheme.colorScheme.background,
+        contentColor = textColor,
+        indicator = { tabPositions ->
+            TabRowDefaults.SecondaryIndicator(
+                modifier = Modifier.tabIndicatorOffset(
+                    tabPositions[selectedTabIndex]
+                ), color = PomoroDoTheme.colorScheme.primaryContainer
+            )
+        }
+    ) {
+        Tab(
+            selected = selectedTabIndex == 0,
+            onClick = { onSelectedTabIndexChange(0) },
+            text = {
+                Text(
+                    text = stringResource(R.string.title_following),
+                    color = textColor,
+                    textAlign = TextAlign.Center,
+                    style = PomoroDoTheme.typography.laundryGothicRegular16
+                )
+            }
+        )
+
+        Tab(
+            selected = selectedTabIndex == 1,
+            onClick = { onSelectedTabIndexChange(1) },
+            text = {
+                Text(
+                    text = stringResource(id = R.string.title_follower),
+                    color = textColor,
+                    textAlign = TextAlign.Center,
+                    style = PomoroDoTheme.typography.laundryGothicRegular16
+                )
+            }
         )
     }
 }
