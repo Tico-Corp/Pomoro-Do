2c90b44523c8a0c51b585192bbedb4a49f7f77d6
souk0712 <souk0712@gmail.com>
2024-07-15 00:02:55 +0900

    [FEAT] Category 데이터 변수 추가 및 임시 데이터 변경
    - Category 데이터 변수 추가
    - 임시 데이터 변경
    
    Fixes: TICO-204

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/model/Category.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/model/Category.kt
@@ -3,6 +3,15 @@ package com.tico.pomorodo.data.model
 data class Category(
     val id: String,
     val title: String,
-    val todoList: List<TodoData>,
-    val groupNumber: Int = 0
+    val type: CategoryType,
+    val todoList: List<TodoData>? = null,
+    val openSettings: OpenSettings = OpenSettings.FULL,
+    val groupNumber: Int = 0,
+    val groupReader: String? = null,
+    val isGroupReader: Boolean? = null,
+    val groupMember: List<User>? = null
 )
+
+enum class CategoryType {
+    NORMAL, GROUP
+}
