f47654849ef9fba95d749d41b754e46de15b3054
souk0712 <souk0712@gmail.com>
2024-07-20 23:23:46 +0900

    [FEAT] 네트워크 통신에 쓰이는 상수들 생성
    Fixes: TICO-191

---
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/common/util/NetworkConstants.kt
@@ -0,0 +1,8 @@
+package com.tico.pomorodo.common.util
+
+object NetworkConstants {
+    const val GOOGLE_ID_TOKEN_NAME = "Google-ID-Token"
+    const val ACCESS_TOKEN_NAME = "Authorization"
+    const val USER_NOT_FOUND = "U-102"
+    const val SUCCESS_STATUS = 200
+}
