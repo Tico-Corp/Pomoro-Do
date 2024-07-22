889cc2e38b716c12ebc6705bbb97fc467ae161e3
souk0712 <souk0712@gmail.com>
2024-07-22 22:59:14 +0900

    [FEAT] 네트워크 통신 변수 추가 및 현재 auth 상태 클래스 추가
    Fixes: TICO-191

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/common/util/NetworkConstants.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/common/util/NetworkConstants.kt
@@ -4,5 +4,6 @@ object NetworkConstants {
     const val GOOGLE_ID_TOKEN_NAME = "Google-ID-Token"
     const val ACCESS_TOKEN_NAME = "Authorization"
     const val USER_NOT_FOUND = "U-102"
-    const val SUCCESS_STATUS = 200
+    const val SUCCESS_LOGIN = 200
+    const val SUCCESS_JOIN = 201
 }
