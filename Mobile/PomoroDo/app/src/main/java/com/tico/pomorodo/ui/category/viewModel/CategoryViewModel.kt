dd42ff73ac8247f57db04d4a10a64e653653296a
souk0712 <souk0712@gmail.com>
2024-08-15 23:15:39 +0900

    [FEAT] CategoryViewModel - hiltViewModel로 변경 - 모든 카테고리를 불러오는 로직 추가
    Fixes: TICO-173

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/category/viewModel/CategoryViewModel.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/category/viewModel/CategoryViewModel.kt
@@ -1,14 +1,23 @@
 package com.tico.pomorodo.ui.category.viewModel
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.tico.pomorodo.data.local.datasource.DataSource
 import com.tico.pomorodo.data.model.Category
 import com.tico.pomorodo.data.model.InviteCategory
+import com.tico.pomorodo.domain.model.Resource
+import com.tico.pomorodo.domain.usecase.GetAllCategoryUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class CategoryViewModel : ViewModel() {
+@HiltViewModel
+class CategoryViewModel @Inject constructor(private val getAllCategoryUseCase: GetAllCategoryUseCase) :
+    ViewModel() {
 
     private var _categoryList = MutableStateFlow<List<Category>>(emptyList())
     val categoryList: StateFlow<List<Category>>
@@ -17,4 +26,35 @@ class CategoryViewModel : ViewModel() {
     private var _inviteGroupCategoryList = MutableStateFlow(DataSource.inviteList)
     val inviteGroupCategoryList: StateFlow<List<InviteCategory>>
         get() = _inviteGroupCategoryList.asStateFlow()
+
+    private var _isLoading = MutableStateFlow<Boolean>(false)
+    val isLoading: StateFlow<Boolean>
+        get() = _isLoading.asStateFlow()
+
+    init {
+        getAllCategory()
+    }
+
+    private fun getAllCategory() = viewModelScope.launch {
+        getAllCategoryUseCase().collect { result ->
+            when (result) {
+                is Resource.Success -> {
+                    _isLoading.value = false
+                    _categoryList.value = result.data
+                }
+
+                is Resource.Loading -> {
+                    _isLoading.value = true
+                }
+
+                is Resource.Failure.Exception -> {
+                    Log.e("CategoryViewModel", "getAllCategory: ${result.message}")
+                }
+
+                is Resource.Failure.Error -> {
+                    Log.e("CategoryViewModel", "getAllCategory: ${result.code} ${result.message}")
+                }
+            }
+        }
+    }
 }
