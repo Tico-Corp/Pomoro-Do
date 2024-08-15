e1a4d51147dd53bf10d85bb08327ffc420af2ad2
souk0712 <souk0712@gmail.com>
2024-08-15 23:17:43 +0900

    [FEAT] Loading 추가
    Fixes: TICO-173

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/domain/model/Resource.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/domain/model/Resource.kt
@@ -2,6 +2,7 @@ package com.tico.pomorodo.domain.model
 
 sealed class Resource<out T> {
     class Success<out T>(val data: T) : Resource<T>()
+    class Loading : Resource<Nothing>()
     sealed class Failure<out T>(val message: String?, val code: String? = null) : Resource<T>() {
         class Error<out T>(message: String?, code: String?) : Failure<T>(message, code)
         class Exception<out T>(message: String?) : Failure<T>(message)
