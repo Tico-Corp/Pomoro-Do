bbaa53fbd79cc0f5007ea36e5aa9f79d5633ac17
souk0712 <souk0712@gmail.com>
2024-08-20 21:56:02 +0900

    [FEAT] auth와 token api service
    Fixes: TICO-133

---
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/remote/service/TokenApiService.kt
@@ -0,0 +1,10 @@
+package com.tico.pomorodo.data.remote.service
+
+import com.tico.pomorodo.data.remote.models.response.BaseResponse
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+interface TokenApiService {
+    @POST("/api/auth/tokens/validation")
+    suspend fun validateToken(@Query("tokenType") tokenType: String): BaseResponse<String>
+}
