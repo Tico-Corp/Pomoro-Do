ce80bd943ed9023b7736108eb92fd3d3a8e636b9
souk0712 <souk0712@gmail.com>
2024-08-20 18:57:57 +0900

    [FEAT] data class 생성 및 errorResponse 분리 - base - baseResponse - authResponse
    Fixes: TICO-133

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/remote/models/request/AuthRequestBody.kt
+++
@@ -1,8 +0,0 @@
-package com.tico.pomorodo.data.remote.models.request
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class UserInfoRequestBody(
-    val nickname: String,
-)
--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/remote/models/response/AuthResponse.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/remote/models/response/AuthResponse.kt
@@ -1,27 +1,9 @@
 package com.tico.pomorodo.data.remote.models.response
 
-import com.tico.pomorodo.data.model.Auth
 import com.tico.pomorodo.data.model.Token
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class AuthResponse(
-    val status: Int,
-    val message: String,
-    val code: String,
-    val data: TokenResponse
-)
+data class TokenResponse(val accessToken: String, val refreshToken: String)
 
-@Serializable
-data class TokenResponse(val accessToken: String)
-
-@Serializable
-data class ErrorResponse(
-    val status: Int,
-    val code: String,
-    val message: String
-)
-
-fun AuthResponse.asModel() = Auth(status = status, message = message, data = data.asModel())
-
-fun TokenResponse.asModel() = Token(accessToken)
+fun TokenResponse.toToken() = Token(accessToken, refreshToken)
