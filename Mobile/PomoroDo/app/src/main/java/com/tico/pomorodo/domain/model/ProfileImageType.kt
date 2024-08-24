9815e3eeff8500c846694660a8a4d588cea271c5
souk0712 <souk0712@gmail.com>
2024-08-24 23:41:53 +0900

    [CHORE] 피드백 반영 - 불필요한 매개변수 삭제
    Fixes: TICO-133

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/domain/model/ProfileImageType.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/domain/model/ProfileImageType.kt
@@ -1,7 +1,7 @@
 package com.tico.pomorodo.domain.model
 
-enum class ProfileImageType(val type: String) {
-    FILE("FILE"),
-    GOOGLE("GOOGLE"),
-    DEFAULT("DEFAULT")
+enum class ProfileImageType {
+    FILE,
+    GOOGLE,
+    DEFAULT
 }
