5ba7ba41b2ca8724ad004fee69584fcbe1ecddb3
김부경 <sa01023@naver.com>
2024-09-16 17:12:30 +0900

    [FEAT] DB 변경에 따른 카테고리 조회 서비스 로직 수정
    날짜별 관리를 위한 date 관련 로직 추가
    
    Resolves: TICO-323
    Related to: TICO-255

--- backend/pomoro-do/src/main/java/com/tico/pomoro_do/domain/category/repository/CategoryRepository.java
+++ backend/pomoro-do/src/main/java/com/tico/pomoro_do/domain/category/repository/CategoryRepository.java
@@ -5,12 +5,13 @@ import com.tico.pomoro_do.domain.user.entity.User;
 import com.tico.pomoro_do.global.enums.CategoryType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    List<Category> findAllByHostAndType(User host, CategoryType type);
+    List<Category> findAllByHostAndTypeAndDate(User host, CategoryType type, LocalDate date);
 
     Optional<Category> findById(Long categoryId);
 }
