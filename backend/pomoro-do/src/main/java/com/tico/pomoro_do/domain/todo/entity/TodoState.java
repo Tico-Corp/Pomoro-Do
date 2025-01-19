d6c1fdba14a7f157a6d0fbc72a35fa8f264a04cc
김부경 <sa01023@naver.com>
2025-01-19 12:58:46 +0900

    [FEAT] Todo 도메인 확장 및 컬럼 수정
    - todos 및 todo_likes 테이블 컬럼 수정
    - todo_states 테이블 추가
    
    Resolves: TICO-373
    Related to: TICO-378

---
+++ backend/pomoro-do/src/main/java/com/tico/pomoro_do/domain/todo/entity/TodoState.java
@@ -0,0 +1,31 @@
+package com.tico.pomoro_do.domain.todo.entity;
+
+import com.tico.pomoro_do.domain.user.entity.User;
+import com.tico.pomoro_do.global.entity.BaseTimeEntity;
+import com.tico.pomoro_do.global.enums.TodoStatus;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "todo_states")
+public class TodoState extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "todo_state_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "todo_id", nullable = false)
+    private Todo todo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TodoStatus status = TodoStatus.TODO;
+
+    @Column(name = "likes_count", nullable = false)
+    private int likesCount = 0;     // 좋아요 수
+}
