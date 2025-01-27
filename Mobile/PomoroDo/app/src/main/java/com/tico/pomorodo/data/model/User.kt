7ff637a102b0bee39ef867c6f0e7b6f773e5f43a
souk0712 <souk0712@gmail.com>
2025-01-27 19:07:40 +0900

    [FEAT] user를 userEntity로 변환하는 mapper 확장함수 추가
    Fixes: TICO-178
    Related to: TICO-173

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/model/User.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/model/User.kt
@@ -1,8 +1,12 @@
 package com.tico.pomorodo.data.model
 
+import com.tico.pomorodo.data.local.entity.UserEntity
+
 data class User(
     val id: Int,
     val email: String,
     val name: String,
     val profileUrl: String? = null
 )
+
+fun User.toUserEntity() = UserEntity(id, email, name, profileUrl)
