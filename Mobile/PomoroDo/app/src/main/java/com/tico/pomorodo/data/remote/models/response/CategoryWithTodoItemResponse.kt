8cde9ade242e934fc317769f23e69d3ff412cbe9
souk0712 <souk0712@gmail.com>
2025-01-28 18:45:55 +0900

    [FEAT] 카테고리 별 투두 응답 객체 및 mapper 확장 함수 추가
    Fixes: TICO-349
    Related to: TICO-173

---
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/remote/models/response/CategoryWithTodoItemResponse.kt
@@ -0,0 +1,27 @@
+package com.tico.pomorodo.data.remote.models.response
+
+import com.tico.pomorodo.data.model.CategoryType
+import com.tico.pomorodo.data.model.CategoryWithTodoItem
+import com.tico.pomorodo.data.model.OpenSettings
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class CategoryWithTodoItemResponse(
+    val categoryId: Int,
+    val title: String,
+    val type: CategoryType,
+    val todoList: List<TodoResponse>,
+    val openSettings: OpenSettings,
+    val groupMemberCount: Int,
+    val groupMember: List<UserResponse>
+)
+
+fun CategoryWithTodoItemResponse.toCategoryWithTodoItem() = CategoryWithTodoItem(
+    categoryId = categoryId,
+    title = title,
+    type = type,
+    todoList = todoList.map(TodoResponse::toTodoData),
+    openSettings = openSettings,
+    groupMemberCount = groupMemberCount,
+    groupMember = groupMember.map(UserResponse::toUser)
+)
