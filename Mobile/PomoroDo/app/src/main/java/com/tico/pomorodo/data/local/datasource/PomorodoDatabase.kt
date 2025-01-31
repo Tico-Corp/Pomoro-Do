3594e1728327ea498e843869f5e7222c808a71c6
souk0712 <souk0712@gmail.com>
2025-01-31 22:37:16 +0900

    [FEAT] calendarDao와 CalendarDateEntity를 데이터베이스에 추가
    Fixes: TICO-179
    Related to: TICO-173

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/local/datasource/PomorodoDatabase.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/local/datasource/PomorodoDatabase.kt
@@ -4,14 +4,16 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.tico.pomorodo.common.util.Converters
+import com.tico.pomorodo.data.local.dao.CalendarDao
 import com.tico.pomorodo.data.local.dao.CategoryDao
 import com.tico.pomorodo.data.local.dao.TodoDao
+import com.tico.pomorodo.data.local.entity.CalendarDateEntity
 import com.tico.pomorodo.data.local.entity.CategoryEntity
 import com.tico.pomorodo.data.local.entity.TodoEntity
 import com.tico.pomorodo.data.local.entity.UserEntity
 
 @Database(
-    entities = [TodoEntity::class, CategoryEntity::class, UserEntity::class],
+    entities = [TodoEntity::class, CategoryEntity::class, UserEntity::class, CalendarDateEntity::class],
     version = 1,
     exportSchema = false
 )
@@ -19,6 +21,7 @@ import com.tico.pomorodo.data.local.entity.UserEntity
 abstract class PomorodoDatabase : RoomDatabase() {
     abstract fun todoDao(): TodoDao
     abstract fun categoryDao(): CategoryDao
+    abstract fun calendarDao(): CalendarDao
 
     companion object {
         const val DATABASE_NAME = "pomorodo_database"
