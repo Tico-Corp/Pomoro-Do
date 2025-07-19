474013e639656dbec3e37992ed5ffc3e6963881b
souk0712 <souk0712@gmail.com>
2025-07-19 14:04:44 +0900

    [REFACTOR] 코드 분리 및 정리
    - AppMainNavHost로 코드 분리
    - AppState에서 navController를 관리하도록 변경하고 mainNavController와 homeNavController로 변수 구분
    
    Fixes: TICO-185

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/MainActivity.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.tico.pomorodo.ui.MainScreen
+import com.tico.pomorodo.ui.home.view.rememberAppState
 import com.tico.pomorodo.ui.theme.PomoroDoTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,9 +13,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val appState = rememberAppState()
+
             PomoroDoTheme {
                 // A surface container using the 'background' color from the theme
-                MainScreen()
+                MainScreen(appState)
             }
         }
     }
--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/MainScreen.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/MainScreen.kt
@@ -12,59 +12,21 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
-import com.tico.pomorodo.navigation.MainNavigationDestination
-import com.tico.pomorodo.navigation.addCategoryScreen
-import com.tico.pomorodo.navigation.addFollowerScreen
-import com.tico.pomorodo.navigation.appThemeScreen
-import com.tico.pomorodo.navigation.breakModeScreen
-import com.tico.pomorodo.navigation.categoryScreen
-import com.tico.pomorodo.navigation.concentrationModeScreen
-import com.tico.pomorodo.navigation.getState
-import com.tico.pomorodo.navigation.groupMemberChooseScreen
-import com.tico.pomorodo.navigation.historyScreen
-import com.tico.pomorodo.navigation.homeScreen
-import com.tico.pomorodo.navigation.infoCategoryScreen
-import com.tico.pomorodo.navigation.logInScreen
-import com.tico.pomorodo.navigation.modifyProfileScreen
-import com.tico.pomorodo.navigation.navigateToAddCategory
-import com.tico.pomorodo.navigation.navigateToAddFollowerScreen
-import com.tico.pomorodo.navigation.navigateToAppThemeScreen
-import com.tico.pomorodo.navigation.navigateToBreakMode
-import com.tico.pomorodo.navigation.navigateToCategory
-import com.tico.pomorodo.navigation.navigateToConcentrationMode
-import com.tico.pomorodo.navigation.navigateToGroupMemberChoose
-import com.tico.pomorodo.navigation.navigateToHistory
-import com.tico.pomorodo.navigation.navigateToHome
-import com.tico.pomorodo.navigation.navigateToInfoCategory
-import com.tico.pomorodo.navigation.navigateToLogIn
-import com.tico.pomorodo.navigation.navigateToModifyProfile
-import com.tico.pomorodo.navigation.navigateToSettingScreen
-import com.tico.pomorodo.navigation.navigateToSignUp
-import com.tico.pomorodo.navigation.setState
-import com.tico.pomorodo.navigation.settingScreen
-import com.tico.pomorodo.navigation.signUpScreen
-import com.tico.pomorodo.navigation.splashScreen
-import com.tico.pomorodo.ui.common.view.BREAK_TIME
-import com.tico.pomorodo.ui.common.view.CONCENTRATION_TIME
+import com.tico.pomorodo.navigation.AppMainNavHost
+import com.tico.pomorodo.ui.home.view.AppState
 import com.tico.pomorodo.ui.theme.PomoroDoTheme
 
 @Composable
-fun MainScreen() {
+fun MainScreen(appState: AppState) {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = PomoroDoTheme.colorScheme.background
     ) {
-        val mainNavController = rememberNavController()
-
         Scaffold(
             modifier = Modifier.fillMaxSize(),
             contentWindowInsets = WindowInsets(0, 0, 0, 0)
         ) { innerPadding ->
-            NavHost(
-                navController = mainNavController,
-                startDestination = MainNavigationDestination.SPLASH.name,
+            AppMainNavHost(
                 modifier = Modifier
                     .fillMaxSize()
                     .padding(innerPadding)
@@ -73,82 +35,9 @@ fun MainScreen() {
                         WindowInsets.safeDrawing.only(
                             WindowInsetsSides.Horizontal,
                         ),
-                    )
-            ) {
-                splashScreen(
-                    navigateToLogin = mainNavController::navigateToLogIn,
-                    navigateToHome = mainNavController::navigateToHome
-                )
-
-                logInScreen(
-                    navigateToSignUp = mainNavController::navigateToSignUp,
-                    navigateToHome = mainNavController::navigateToHome,
-                )
-                signUpScreen(
-                    navController = mainNavController,
-                    navigateToHome = mainNavController::navigateToHome,
-                    navigateToBack = mainNavController::popBackStack
-                )
-
-                homeScreen(
-                    setTimerState = { concentrationTime, breakTime ->
-                        mainNavController.setState(CONCENTRATION_TIME, concentrationTime)
-                        mainNavController.setState(BREAK_TIME, breakTime)
-                    },
-                    navigateToConcentrationMode = mainNavController::navigateToConcentrationMode,
-                    navigateToBreakMode = mainNavController::navigateToBreakMode,
-                    navigateToCategory = mainNavController::navigateToCategory,
-                    navigateToAddCategory = mainNavController::navigateToAddCategory,
-                    navigateToHistory = mainNavController::navigateToHistory,
-                    navigateToModifyProfile = mainNavController::navigateToModifyProfile,
-                    navigateToSettingScreen = mainNavController::navigateToSettingScreen,
-                    navigateToAddFollowerScreen = mainNavController::navigateToAddFollowerScreen,
-                )
-
-                concentrationModeScreen(
-                    popBackStack = mainNavController::popBackStack,
-                    getState = mainNavController::getState,
-                    navigateToBreakMode = mainNavController::navigateToBreakMode
-                )
-                breakModeScreen(
-                    mainNavController = mainNavController,
-                    getState = mainNavController::getState
-                )
-
-                categoryScreen(
-                    navigateToAddCategory = mainNavController::navigateToAddCategory,
-                    navigateToBack = mainNavController::popBackStack,
-                    navigateToInfoCategory = { categoryId ->
-                        mainNavController.navigateToInfoCategory(
-                            categoryId = categoryId
-                        )
-                    }
-                )
-                addCategoryScreen(
-                    navigateToGroupMemberChoose = mainNavController::navigateToGroupMemberChoose,
-                    navigateToBack = mainNavController::popBackStack
-                )
-                infoCategoryScreen(
-                    navigateToGroupMemberChoose = mainNavController::navigateToGroupMemberChoose,
-                    navigateToBack = mainNavController::popBackStack
-                )
-                groupMemberChooseScreen(
-                    navController = mainNavController,
-                    navigateToBack = mainNavController::popBackStack
-                )
-
-                historyScreen(navigateToBack = mainNavController::popBackStack)
-
-                modifyProfileScreen(navController = mainNavController)
-
-                settingScreen(
-                    navigateToAppThemeScreen = mainNavController::navigateToAppThemeScreen,
-                    popBackStack = mainNavController::popBackStack
-                )
-                appThemeScreen(popBackStack = mainNavController::popBackStack)
-
-                addFollowerScreen(popBackToFollowScreen = mainNavController::popBackStack)
-            }
+                    ),
+                appState = appState
+            )
         }
     }
 }
