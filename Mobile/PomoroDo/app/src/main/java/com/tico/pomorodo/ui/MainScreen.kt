537c0d51c4ab31bf6c28b959534f3a99ad3ef03c
souk0712 <souk0712@gmail.com>
2025-08-12 19:53:23 +0900

    [FEAT] 네트워크 스낵바 표시
    - 네트워크 연결 상태이며, 오프라인 모드라면 스낵바를 표시
    
    Related to: TICO-174
    Fixes: TICO-415

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/MainScreen.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/MainScreen.kt
@@ -1,5 +1,6 @@
 package com.tico.pomorodo.ui
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
 import androidx.compose.foundation.layout.consumeWindowInsets
@@ -9,8 +10,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import com.tico.pomorodo.R
 import com.tico.pomorodo.navigation.AppNavHost
 import com.tico.pomorodo.ui.home.view.BottomBar
 import com.tico.pomorodo.ui.home.view.rememberAppState
@@ -20,6 +28,31 @@ import com.tico.pomorodo.ui.theme.PomoroDoTheme
 fun MainScreen() {
     val appState = rememberAppState()
     val homeTabs = appState.bottomNavigationDestinationList.map { it.name }.toSet()
+    val context = LocalContext.current
+
+    LaunchedEffect(key1 = appState.isNetworkConnected.value, key2 = appState.isOffline.value) {
+        if (appState.isNetworkConnected.value) {
+            if (appState.isOffline.value) {
+                val result = appState.snackBarHostState.showSnackbar(
+                    withDismissAction = true,
+                    message = context.getString(R.string.title_connected_network_change_online_mode),
+                    actionLabel = context.getString(R.string.content_change_online_mode),
+                    duration = SnackbarDuration.Indefinite
+                )
+                if (result == SnackbarResult.ActionPerformed) {
+                    // TODO: Sync 화면으로 이동
+                    appState.setIsOffline(false)
+                }
+            }
+        } else {
+            Toast.makeText(
+                appState.navController.context,
+                context.getString(R.string.title_disconnected_network),
+                Toast.LENGTH_LONG
+            ).show()
+            appState.setIsOffline(true)
+        }
+    }
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
@@ -30,7 +63,18 @@ fun MainScreen() {
                 BottomBar(appState)
             }
         },
-        containerColor = PomoroDoTheme.colorScheme.background
+        containerColor = PomoroDoTheme.colorScheme.background,
+        snackbarHost = {
+            SnackbarHost(hostState = appState.snackBarHostState, snackbar = {
+                Snackbar(
+                    snackbarData = it,
+                    containerColor = PomoroDoTheme.colorScheme.gray10,
+                    contentColor = PomoroDoTheme.colorScheme.onPrimary,
+                    actionColor = PomoroDoTheme.colorScheme.primaryContainer,
+                    dismissActionContentColor = PomoroDoTheme.colorScheme.gray50
+                )
+            })
+        }
     ) { innerPadding ->
         AppNavHost(
             modifier = Modifier
