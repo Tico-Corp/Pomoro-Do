90358e4f61f37dd3e4035e2e1abe73fd48cc6af9
River is line <dmstjs7047@hanmail.net>
2025-09-08 16:04:27 +0900

    [ERROR] 프로필이 기본 이미지일때 서버에서 받아오는 이미지 url이 null이어서 발생하는 오류 해결
    profileImageUrl 변수를 nullable하게 변경
    
    Fixes: TICO-434
    Related to: TICO-432

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/model/UserProfile.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/model/UserProfile.kt
@@ -2,7 +2,7 @@ package com.tico.pomorodo.data.model
 
 data class UserProfile(
     val nickname: String,
-    val profileImageUrl: String,
+    val profileImageUrl: String?,
     val followingCount: Int,
     val followerCount: Int
 )
