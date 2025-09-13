cd07ef00528bf764380bd09df42d3dcb6556a460
souk0712 <souk0712@gmail.com>
2025-09-13 22:18:41 +0900

    [FEAT] 팔로우 투두 viewModel에서 myUserId 변수 선언 및 데이터 불러오는 로직 연결
    Fixed: TICO-431

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/todo/viewmodel/FollowTodoViewModel.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/todo/viewmodel/FollowTodoViewModel.kt
@@ -1,16 +1,21 @@
 package com.tico.pomorodo.ui.todo.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.tico.pomorodo.data.model.CalendarDate
 import com.tico.pomorodo.data.model.CategoryWithTodoItem
 import com.tico.pomorodo.domain.model.Follow
+import com.tico.pomorodo.domain.model.Resource
+import com.tico.pomorodo.domain.usecase.user.GetMyUserIdUseCase
 import com.tico.pomorodo.navigation.UserArgs
 import com.tico.pomorodo.ui.common.view.toTimeZoneOf5AM
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import kotlinx.datetime.Clock
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.TimeZone
@@ -18,7 +23,10 @@ import kotlinx.datetime.toLocalDateTime
 import javax.inject.Inject
 
 @HiltViewModel
-class FollowTodoViewModel @Inject constructor(saveStateHandle: SavedStateHandle) : ViewModel() {
+class FollowTodoViewModel @Inject constructor(
+    saveStateHandle: SavedStateHandle,
+    private val getMyUserIdUseCase: GetMyUserIdUseCase
+) : ViewModel() {
 
     private val args = UserArgs(saveStateHandle)
 
@@ -26,6 +34,10 @@ class FollowTodoViewModel @Inject constructor(saveStateHandle: SavedStateHandle)
         MutableStateFlow<List<CategoryWithTodoItem>>(emptyList())
     val categoryWithTodoItemList: StateFlow<List<CategoryWithTodoItem>>
         get() = _categoryWithTodoItemList.asStateFlow()
+
+    private var _myUserId = MutableStateFlow<Int>(-1)
+    val myUserId: StateFlow<Int>
+        get() = _myUserId.asStateFlow()
 
     private var _user = MutableStateFlow<Follow?>(null)
     val user: StateFlow<Follow?>
@@ -65,6 +77,32 @@ class FollowTodoViewModel @Inject constructor(saveStateHandle: SavedStateHandle)
     private fun loadData() {
         fetchFollowInfo(args.userId)
         getCategoryWithTodoItems(args.userId)
+        getMyUserId()
+    }
+
+    private fun getMyUserId() = viewModelScope.launch {
+        getMyUserIdUseCase().collect { result ->
+            when (result) {
+                is Resource.Loading -> {
+
+                }
+
+                is Resource.Success -> {
+                    _myUserId.value = result.data
+                }
+
+                is Resource.Failure.Exception -> {
+                    Log.e("FollowTodoViewModel", "getMyUserId: ${result.message}")
+                }
+
+                is Resource.Failure.Error -> {
+                    Log.e(
+                        "FollowTodoViewModel",
+                        "getMyUserId: ${result.code} ${result.message}"
+                    )
+                }
+            }
+        }
     }
 
     fun setSelectedDate(date: LocalDate) {
