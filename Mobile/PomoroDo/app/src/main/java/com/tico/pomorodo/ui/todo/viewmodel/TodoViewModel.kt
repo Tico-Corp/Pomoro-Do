98f6f9dbb73184d44fe3a244313f6f2d16a003c3
souk0712 <souk0712@gmail.com>
2025-09-13 22:07:02 +0900

    [FEAT] 내 userId와 전달 받은 userId가 같은지 반환하는 함수
    Fixed: TICO-431

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/todo/viewmodel/TodoViewModel.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/ui/todo/viewmodel/TodoViewModel.kt
@@ -240,4 +240,8 @@ class TodoViewModel @Inject constructor(
             deleteTodoUseCase(todoId)
         }
     }
+
+    fun isMyUserId(userId: Int): Boolean {
+        return myUserId.value == userId
+    }
 }
