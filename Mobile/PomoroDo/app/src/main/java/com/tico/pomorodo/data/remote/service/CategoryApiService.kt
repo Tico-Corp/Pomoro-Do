4b5510a45d3c8d07d84da25062e001348a195db2
souk0712 <souk0712@gmail.com>
2025-09-26 03:56:34 +0900

    [FEAT] 카테고리 관련 api service 추가
    - getAllCategory: 모든 카테고리 목록 조회
    - getCategoryInfo: 특정 카테고리 정보 조회
    - insertCategory: 카테고리 생성
    - updateCategory: 카테고리 수정
    - deleteCategory: 카테고리 삭제
    
    Fixed: TICO-450

---
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/remote/service/CategoryApiService.kt
@@ -0,0 +1,37 @@
+package com.tico.pomorodo.data.remote.service
+
+import com.tico.pomorodo.data.remote.models.request.CategoryDeleteRequest
+import com.tico.pomorodo.data.remote.models.request.CategoryRequest
+import com.tico.pomorodo.data.remote.models.request.CategoryUpdateRequest
+import com.tico.pomorodo.data.remote.models.response.AllCategoryResponse
+import com.tico.pomorodo.data.remote.models.response.BaseResponse
+import com.tico.pomorodo.data.remote.models.response.CategoryResponse
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.PATCH
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface CategoryApiService {
+    @GET("v1/categories")
+    suspend fun getAllCategory(): BaseResponse<AllCategoryResponse>
+
+    @GET("v1/categories/{categoryId}")
+    suspend fun getCategoryInfo(@Path("categoryId") categoryId: Int): BaseResponse<CategoryResponse>
+
+    @POST("v1/categories")
+    suspend fun insertCategory(@Body categoryRequest: CategoryRequest)
+
+    @PATCH("v1/categories/api/v1/categories/{categoryId}")
+    suspend fun updateCategory(
+        @Path("categoryId") categoryId: Int,
+        @Body categoryUpdateRequest: CategoryUpdateRequest
+    )
+
+    @DELETE("v1/categories/{categoryId}")
+    suspend fun deleteCategory(
+        @Path("categoryId") categoryId: Int,
+        @Body categoryDeleteRequest: CategoryDeleteRequest
+    )
+}
