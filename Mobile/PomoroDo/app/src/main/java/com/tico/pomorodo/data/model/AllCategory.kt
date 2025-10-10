3e7d370dfa117507f1fa902201e4fcf57fb3a9de
souk0712 <souk0712@gmail.com>
2025-10-10 21:55:43 +0900

    [FEAT] categoryEntity로 변환하는 확장 함수 정의
    Fixed: TICO-450

--- Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/model/AllCategory.kt
+++ Mobile/PomoroDo/app/src/main/java/com/tico/pomorodo/data/model/AllCategory.kt
@@ -1,5 +1,7 @@
 package com.tico.pomorodo.data.model
 
+import com.tico.pomorodo.data.local.entity.CategoryEntity
+
 data class AllCategory(
     val personalCategories: List<CategoryList>,
     val groupCategories: List<CategoryList>,
@@ -17,4 +19,18 @@ data class CategoryInvitation(
     val categoryInvitationId: Int,
     val categoryName: String,
     val ownerNickname: String
+)
+
+fun AllCategory.toCategoryEntity(): List<CategoryEntity> =
+    personalCategories.map(CategoryList::toCategoryEntity) + groupCategories.map(CategoryList::toCategoryEntity)
+
+fun CategoryList.toCategoryEntity() = CategoryEntity(
+    id = categoryId,
+    title = categoryName,
+    type = type,
+    totalMembers = totalMembers,
+    openSettings = when (type) {
+        CategoryType.PERSONAL -> OpenSettings.PUBLIC
+        CategoryType.GROUP -> OpenSettings.GROUP
+    }
 )
